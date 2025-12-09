e1996dc0e3309519b4b7bbadbe27186501745a0b
David Tracy <david.tracy3@gmail.com>
2025-12-09 07:54:16 -0800

    Day 1 Part 1

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,47 @@
 const std = @import("std");
 const aoc_2025 = @import("aoc_2025");
+const fmt = std.fmt;
+const fs = std.fs;
+const mem = std.mem;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try aoc_2025.bufferedPrint();
+    try day1();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+pub fn day1() !void {
+    const input = @embedFile("input/day1.txt");
+    var zero_count: u32 = 0;
+    var pos: u32 = 50;
+    var it = mem.splitAny(u8, input, "\n");
+    const move = it.first();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    pos = try processMove(move, pos);
+    if (pos == 0) {
+        zero_count += 1;
+    }
+    while (it.next()) |move2| {
+        pos = try processMove(move2, pos);
+        if (pos == 0) {
+            zero_count += 1;
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    }
+    std.debug.print("Part 1: {}\n", .{zero_count});
+}
+
+const ProcessMoveError = error{InvalidMove};
+
+fn processMove(move: []const u8, pos: u32) !u32 {
+    const dir = move[0];
+    const len = fmt.parseInt(u32, move[1..], 10) catch 0;
+
+    if (dir == 'R') {
+        return (pos + len) % 100;
+    } else if (dir == 'L') {
+        const len_capped = len % 100;
+        if (len_capped > pos) {
+            return 100 - (len_capped - pos);
+        }
+        return pos - len_capped;
+    }
+    return error.InvalidMove;
 }
